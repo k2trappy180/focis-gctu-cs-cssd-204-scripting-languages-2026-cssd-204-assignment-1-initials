4c78dc92944204787e34c836e7946da3ebff5268
Nyamekye Sarfo-Mensah <2425401408@live.gctu.edu.gh>
2026-05-31 23:33:26 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,27 @@
 
 def main():
-  pass #code goes here
+   #File:initials.py
+    #Description:Print out my initials
+    #Assignment Number: 2
+    #
+    #Name:Kweku Nyamekye Sarfo-Mensah
+    #STUDENT ID:2425401408
+    #Email:2425401408@live.gctu.edu.gh
+    #Grader:Mr.Augustus Buckman
+    #On my honour, Kweku Nyamekye Sarfo Mensah this programming assignment is my own work
+    #and i have not provided this code to any other student
+    print('...KSM')
+    print(       )
+    print('...KK........KK........SSSSSSSSSSSS........MMMM....MMMM.....')
+    print('...KK.......KK.........SS..................MMMM....MMMM.....')
+    print('...KK......KK..........SS..................MM.MM..MM.MM.....')
+    print('...KK.....KK...........SS..................MM.MM..MM.MM.....')
+    print('...KK....KK............SSSSSSSSSSSS........MM.MM..MM.MM.....')
+    print('...KKKKKK..............SSSSSSSSSSSS........MM..MMMM..MM.....')
+    print('...KK....KK......................SS........MM..MMMM..MM.....')
+    print('...KK.....KK.....................SS........MM...MM...MM.....')
+    print('...KK......KK.....**.............SS...**...MM...MM...MM...**')
+    print('...KK.......KK....**...SSSSSSSSSSSS...**...MM...MM...MM...**')
 
 
 
